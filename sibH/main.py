f67dcda66e2701fb0e339a15c42c9ca3c76d36f4
Nathan Hudson <nathanhudson334@gmail.com>
2023-07-20 15:12:24 -0400

    added typing

--- sibH/main.py
+++ sibH/main.py
@@ -50,6 +50,97 @@ closers = [']','}',')']
 breaks = [':',";"]
 chars = ["'",'"',".",","]
 selector = '@'
+special = ["!","#","$","%","^","&","*"]
+
+functions= ['attrs','inner']
+
+tags=["!DOCTYPE",
+"html",
+"head",
+"title",
+"meta",
+"link",
+"script",
+"style",
+"body",
+"h1",
+"h2",
+"h3",
+"h4",
+"h5",
+"h6",
+"p",
+"br",
+"hr",
+"a",
+"img",
+"ul",
+"ol",
+"li",
+"dl",
+"dt",
+"dd",
+"table",
+"tr",
+"th",
+"td",
+"thead",
+"tbody",
+"tfoot",
+"caption",
+"div",
+"span",
+"form",
+"input",
+"textarea",
+"button",
+"select",
+"option",
+"label",
+"fieldset",
+"legend",
+"iframe",
+"audio",
+"video",
+"canvas",
+"svg",
+"nav",
+"header",
+"footer",
+"section",
+"article",
+"aside",
+"main",
+"address",
+"time",
+"abbr",
+"blockquote",
+"q",
+"cite",
+"em",
+"strong",
+"i",
+"b",
+"s",
+"u",
+"sub",
+"sup",
+"code",
+"pre",
+"small",
+"big",
+"mark",
+"del",
+"ins",
+"progress",
+"meter",
+"details",
+"summary",
+"menu",
+"menuitem",
+"dialog"
+
+]
 
 nonLetters = [openers,closers,breaks,chars,selector]
 
@@ -69,10 +160,17 @@ def createStack(file):
 # Tokenizes words and special characters
 def tokenize(cStack):
     for item in cStack:
+        if ord(item) == 58 or ord(item) == 59 or ord(item) == 39 or ord(item) in special or ord(item) in openers or ord(item) in closers:
+            temp = (''.join(tStack).strip(whitespaces))
+            vStack.append(temp)
+            temp = ''
+            tStack.clear()
+            tStack.append(item)
+            temp = (''.join(tStack).strip())
+            tStack.clear()
         if ord(item) != 32 and ord(item) != 40 and ord(item) != 41 and ord(item) != 123 and ord(item) != 125 : #letters and numbers and 
-            tStack.append(item)
-            
-        if ord(item) == 40 or ord(item) == 41 or ord(item) == 123 or ord(item) == 125:
+            tStack.append(item)  
+        if ord(item) == 40 or ord(item) == 41 or ord(item) == 123 or ord(item) == 125 or ord(item) == 39 or ord(item) in special or ord(item) in openers or ord(item) in closers:
             temp = (''.join(tStack).strip(whitespaces))
             vStack.append(temp)
             temp = ''
@@ -82,17 +180,95 @@ def tokenize(cStack):
             vStack.append(temp)
             temp = ''
             tStack.clear()
-        if ord (item) == 32 or ord(item) == 10:
+        if ord (item) == 32 or ord(item) == 10 or ord(item) in special or ord(item) in openers or ord(item) in closers:
             temp = (''.join(tStack).strip(whitespaces))
             vStack.append(temp)
             temp = ''
             tStack.clear()
         
     return vStack
-    
-    
-    
 
+def setType(cont):
+            if cont in functions:
+                return 'Functiion'
+            elif cont in tags:
+                return 'Tag'
+            elif cont in openers:
+                return 'Opener'
+            elif cont in closers:
+                return 'Closer'
+            elif cont == selector:
+                return 'Selector'
+            elif cont in chars:
+                return "Char"
+            elif cont in breaks:
+                return 'Break'
+            elif cont in tags:
+                return 'Tag'
+            elif cont.isalpha():
+                return 'Alpha'
+            elif cont.isdigit():
+                return 'Digit'
+            elif cont in whitespaces:
+                return 'Whitespace'
+            elif '://www' in cont:
+                return 'Link'
+            elif 'https' in cont:
+                return 'Protocol'
+
+# Turns string Tokens into Token Objects
+def GenTok(vStack):
+    counter = 0
+    temp = []
+    hop = []
+    for i in range(len(vStack)):
+        j = Token(vStack[i],vStack[i-1],None, setType(vStack[i]))
+        temp.append(j)
+        counter+=1
+    counter = 0
+    for i in temp:
+        if i.typeOf != "Whitespace":
+            hop.append(temp[counter])
+        counter +=1
+    vStack.clear()
+    for i in temp:
+        vStack.append(i)
+    return vStack 
+
+# def genComps(vStack):
+#     counter = 0
+#     temp = []
+#     for i in vStack:
+#         if i in tags:
+#             j = tag(tags[counter])
+#             temp.append(j)
+#         counter+=1
+#     return temp
+    
+class Token:
+    def __init__(self,cont, prevT,nextT, typeOf):
+        self.cont = cont
+        self.prevT = prevT
+        self.nextT = nextT
+        self.typeOf = typeOf
+    def __repr__(self) -> str:
+        return f'Token is:   -  {self.cont}  -   , \t , Previous is {self.prevT} , \t , Next is {self.nextT}, \t ,Type is: {self.typeOf} \n\n'
+
+class tag:
+    def __init__(self, tag) -> None:
+        self.tag = tag
+
+
+class Element():
+    def __init__(self, tag, ELid,attributes,innercontent, ELclass):
+        self.tag = tag
+        self.ELid = ELid
+        self.attrubtes = attributes
+        self.innerContent = innercontent
+        self.ELclass = ELclass
+    def genEL(tag, ELid,attributes,innercontent, ELclass):
+        return f'<{tag} id={ELid} class={ELclass} {attributes}>{innercontent}</{tag}'
+        
 
 
 def main():
@@ -101,7 +277,10 @@ def main():
         try:
             createStack(file)
             tokenize(cStack)
+            GenTok(vStack)
+            # genComps(vStack)
             print(vStack)
+            # code to write out to html file here
             return sys.exit(0)
         except Exception as e:
             raise e
